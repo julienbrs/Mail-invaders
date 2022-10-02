82fcd84561f9c39930d34bcdf00dea265fa3552b
julienbrs <106234742+julienbrs@users.noreply.github.com>
2022-10-03 00:27:53 +0200

    Laser Moving
    Laser moving ok, need clean code + offscreen

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ var panePlayer = $('#player_zone'),
     dataKeyPressed = {},
     tickMovement = 10;
 
+
 function newWidth(oldValue,key1,key2) {
     /* newPos is the new potential topleft */
     var newPos = parseInt(oldValue, 10) 
@@ -21,20 +22,59 @@ function newHeight(oldValue,key1,key2) {
     return newPos < 0 ? 0 : (newPos > maxHeight ? maxHeight : newPos);
 }
 
-$(window).keydown(function(e) { dataKeyPressed[e.which] = true; }); // adding key to array when pressed
+document.addEventListener('keydown', (e) => {
+    if (e.repeat) {
+        return}; // do nothing
+    const {key} = e;
+
+    switch (key) { 
+        case ' ': // spacebar
+            console.log("SHOOTING");
+            player.shoot();
+            break;
+        default:
+            console.log(key);
+    }
+});
+
+
+
+
+$(window).keydown(function(e) {
+    if (e.which == 32) {return;    // do nothing when spacebar
+    }
+    else{                               // adding key to array when pressed 
+        dataKeyPressed[e.which] = true; }
+    });
+
 $(window).keyup(function(e) { dataKeyPressed[e.which] = false; });  // removing key from array when released
 
 
+function getTopLeft(element) {
+    var rect = element.getBoundingClientRect();
+    return {
+        x: rect.left + window.scrollX,
+        y: rect.top + window.scrollY
+    };
+}
+
+function updatePositionPlayer(){
+    var elem = document.getElementById("hitbox_player");
+    var pos = getTopLeft(elem);
+    player.x = pos.x;
+    player.y = pos.y;
+}
+
 
 class Laser {
-    constructor(x, y, width, height, color, speed, image) {
+    constructor(x, y, width, height, color, speed, elem) {
         this.x = x;
         this.y = y;
         this.width = width;
         this.height = height;
         this.color = color;
         this.speed = speed;
-        this.image = image;
+        this.elem = elem;
     }
 
     draw() {
@@ -42,13 +82,15 @@ class Laser {
     }
 
     update() {
-        this.draw();
-        this.x += this.speed;
+        this.elem.style.position = "absolute";
+        //this.style.left = this.x + "px";
+        this.elem.style.top = parseInt(this.elem.style.top) - 2 + "px";
+        // this.x += this.speed;
     }
 }
 
 class Shooter {
-    constructor(x, y, width, height, color, speed) {
+    constructor(x, y, color, speed) {
         this.x = x;
         this.y = y;
         this.lasers = [];
@@ -56,25 +98,39 @@ class Shooter {
 
     draw(board) {
         /* board is the canvas */
-        var canvas = document.getElementById(board);
-        canvas.style.backgroundColor = 'red';
+        // var canvas = document.getElementById(board);
+        // canvas.style.backgroundColor = 'red';
     }
 
     shoot() {
+        /* html of the new laser */
         var img = "assets/laser.png";
-        const screenplay = document.getElementById("screenplay");
         const newLaser = document.createElement("div");
         newLaser.classList.add("laser");
-        newLaser.style.zIndex = 1;
+        const screenplay = document.getElementById("screenplay");
         screenplay.appendChild(newLaser);
         const imgLaser = document.createElement("img");
         imgLaser.src = img;
         newLaser.appendChild(imgLaser);
-        this.lasers.push(new Laser(this.x, this.y, 10, 5, 'blue', 5));
+        this.lasers.push(new Laser(this.x, this.y, 10, 5, 'blue', 5, newLaser));
+        /* CSS of the new laser */
+        newLaser.style.zIndex = 5;
+        newLaser.style.position = "absolute";
+        newLaser.style.left = this.x + "px";
+        newLaser.style.top = this.y + "px";
+        
+        console.log(newLaser);
+    }
+
+    moveLaser() {
+        this.lasers.forEach(laser => {
+            laser.update();
+        });
     }
 }
 
-let player = new Shooter( $('#hitbox_player').x,  $('#hitbox_player').y,  $('#hitbox_player').width,  $('#hitbox_player').height,  'red',  10);
+let player = new Shooter( getTopLeft(document.getElementById("hitbox_player")).x,
+getTopLeft(document.getElementById("hitbox_player")).y,  'red',  10);
 
 
 setInterval(function() {
@@ -83,8 +139,11 @@ setInterval(function() {
         top: function(i,oldValue) { return newHeight(oldValue, 38, 40); }
     });
     player.draw("hitbox_player");
-
+    updatePositionPlayer();
+    if (dataKeyPressed[32]) {
+        player.shoot();
+    }
+    player.moveLaser();
 }, 20); // 20ms is the refresh rate
 
-player.shoot();
-console.log(player.lasers);
+console.log(player);
